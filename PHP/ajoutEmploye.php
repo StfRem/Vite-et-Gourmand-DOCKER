14897d854eb9ba3f390764c861f8b0b0b01bca8f
StfRem <stephaneremiatt54@gmail.com>
2026-03-02 13:51:57 +0100

    Suppression des "ID" et USR....  (fonctionne moi avec l'auto incrémentation)

--- PHP/ajoutEmploye.php
+++ PHP/ajoutEmploye.php
@@ -8,11 +8,11 @@ $data = json_decode(file_get_contents("php://input"), true);
 $fullname = $data["fullname"];
 $email = $data["email"];
 $password = password_hash($data["password"], PASSWORD_BCRYPT);
-$id = "EMP-" . time();
 
 try {
-    $sql = $pdo->prepare("INSERT INTO users (id, fullname, email, password, role) VALUES (?, ?, ?, ?, 'employe')");
-    $ok = $sql->execute([$id, $fullname, $email, $password]);
+    $sql = $pdo->prepare("INSERT INTO users (fullname, email, password, role) VALUES (?, ?, ?, 'employe')");
+    
+    $ok = $sql->execute([$fullname, $email, $password]);
 
     echo json_encode(["success" => $ok]);
 } catch (Exception $e) {
